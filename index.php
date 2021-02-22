85ca0967d1dbcc19d7268e4b93ef524fe208c975
christinapan98 <christina.pan@wustl.edu>
2021-02-22 01:41:20 -0600

    can edit stories

--- index.php
+++ index.php
@@ -10,58 +10,54 @@ session_start();?>
     <body>
     
     	<div class="container">
-    		<div class="news-box">
-                <h1>Your News</h1>
-    			<div class="news">
-                    <?php
-                        // get the database handler
-                        $mysqli = connectdb(); 
-                        // Fetch news
-                        $stmt = $mysqli->prepare("SELECT newstitle, newstext from stories");
+            <h1>Your News</h1>
+            <div class="news">
+                <?php
+                    // get the database handler
+                    $mysqli = connectdb(); 
+                    // Fetch news
+                    // $stmt = $mysqli->prepare("SELECT newstitle, newstext from stories");
 
-                        if(!$stmt){
-                            printf("Query Prep Failed: %s\n", $mysqli->error);
-                            exit;
-                        }
+                    // if(!$stmt){
+                    //     printf("Query Prep Failed: %s\n", $mysqli->error);
+                    //     exit;
+                    // }
 
-                        $stmt->execute();
+                    // $stmt->execute();
 
-                        $stmt->bind_result($title, $text);
+                    // $stmt->bind_result($title, $text);
+                    $allstories = mysqli_query($mysqli, "SELECT * from stories");
 
-                        echo "<ul>\n";
+                    echo "<ul>\n";
 
-                        //this prints out all the stories!
-                        while($stmt->fetch()){
-                            printf("\t<li>%s %s</li>\n",
-                                htmlspecialchars($title),
-                                htmlspecialchars($text)
-                            );
-                    ?>
-                    <!-- inserting php is like diving in and out of the water
-                    while swimming, even if you break out of the tags for a bit
-                the same variables are still accessible in the same file -->
-                            <tr>
-                                <td><?php echo $data['id']; ?></td>
-                                <td><?php echo $data['fullname']; ?></td>
-                                <td><?php echo $data['age']; ?></td>    
-                                <td><a href="edit.php?id=<?php echo $data['id']; ?>">Edit</a></td>
-                                <td><a href="delete.php?id=<?php echo $data['id']; ?>">Delete</a></td>
-                            </tr>	
-                    <?php
-                        }
-                        echo "</ul>\n";
+                    //this prints out all the stories!
+                    while($data = mysqli_fetch_array($allstories)){
+                        // printf("\t<li>%s %s</li>\n",
+                        //     htmlspecialchars($title),
+                        //     htmlspecialchars($text)
+                        // );
+                        echo $data['newstitle'];
+                        echo $data['newstext'];
+                ?>
+                        <!-- inserting php is like diving in and out of the water
+                        while swimming, even if you break out of the tags for a bit
+                        the same variables are still accessible in the same file -->
+                            
+                        <a href="news-item-edit.php?id=<?php echo $data['newsid'];?>">Edit</a>
+                        <a href="delete.php?id=<?php echo $data['newsid']; ?>">Delete</a>
+                <?php
+                    }
+                    echo "</ul>\n";
 
-                        $stmt->close();
-                    ?>
+                    $stmt->close();
+                ?>
 
-                    
-    			</div>
+                
+            </div>
 
-                <div class="newstorysubmit">
-                    <a href="submitnews.php">Submit a New Story</a>
-                </div>
-
-    		</div>
+            <div class="newstorysubmit">
+                <a href="submitnews.php">Submit a New Story</a>
+            </div>
 
     	</div>
 
---
+++ news-item-edit.php
@@ -0,0 +1,44 @@
+<?php 
+	require "database.php"; 
+	// get the database handler
+	$mysqli = connectdb(); 
+	$id = $_GET['id']; // get id through query string which is passed to this file from index.php
+							//done by "posting" the variable to the url
+
+	 // select query, there will only be one that matches the id
+	$query = mysqli_query($mysqli,"select * from stories where id='$id'");
+
+	$data = mysqli_fetch_array($query); // fetch data
+
+	if(isset($_POST['update'])) // when you click on update button
+	{
+		//extract the new values
+		$brandnewtitle = $_POST['title'];
+		$brandnewstory = $_POST['story'];
+		
+		//perform a query and update using those values
+		$edit = mysqli_query($mysqli,"UPDATE stories SET newstitle='$brandnewtitle', newstext='$brandnewstory' WHERE newsid='$id'");
+		
+		if($edit)
+		{
+			mysqli_close($mysqli); // Close connection
+			header("location: index.php"); // redirects to home page
+			exit;
+		} else {
+			echo mysqli_error();
+		}    	
+	}
+?>
+
+<h3>Edit Story</h3>
+
+<form method="POST">
+  <label for="title">New title: </label>
+  <input type="text" id="title" name="title" value="<?php echo $title?>" placeholder="Enter title" Required>
+
+  <label for="story">New story</label>
+  <input type="text" id="story" name="story" value="<?php echo $text?>" placeholder="Enter story..." Required>
+
+  <button type="submit" name="update" value="Update">Submit</button>
+</form>
+
